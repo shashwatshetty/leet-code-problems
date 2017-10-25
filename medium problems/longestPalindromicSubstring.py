2661e2d5c3453bbe70c94e9b6a6b1e489fefe566
shashwatshetty <shashwat_shetty@hotmail.com>
2017-10-24 23:20:53 -0400

    longestPalindromicSubstring failing due to time complexity

--- medium problems/longestPalindromicSubstring.py
+++ medium problems/longestPalindromicSubstring.py
@@ -20,30 +20,17 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        '''
+        print len(s)
         if len(s) == 0:
-            return ''
-        start = 0
-        end = 0
-        subStr = s[start:end]
-        indx = 0
-        longest = end - start
-        longestPal = ''
-        while indx < len(s):
-            if not Solution().isPalindrome(subStr):
-                start += 1
-                indx -= 1
-            else:
-                end += 1
-            indx += 1
-            if longest < end - start:
-                longest = end - start
-                longestPal = s[start:end]
-            subStr = s[start:end]
-        '''
-        for start in range(len(s)):
-            for end in range(len(s),0,-1):
+            return s
+        diff = len(s)
+        while diff > 0:
+            start = 0
+            while start + diff <= len(s):
+                end = start + diff
                 subStr = s[start:end]
                 if Solution().isPalindrome(subStr):
                     return subStr
-        return subStr
+                start += 1
+            diff -= 1
+            
