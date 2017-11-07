00511ad84979b58eb8c7102be551fc3b9b27a8b4
shashwatshetty <shashwat_shetty@hotmail.com>
2017-11-06 19:36:26 -0500

    containerWithMostWater failing due to time complexity

---
+++ medium problems/containerWithMostWater.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        ordinates = []      
+        i = 1
+        while i <= len(height):
+            coord = (i, height[i-1])
+            ordinates.append(coord)
+            i += 1
+        mArea = - float('inf')
+        for x1, y1 in ordinates:
+            for x2, y2 in ordinates:
+                h = min(y1, y2)
+                b = abs(x1 - x2)
+                mArea = max(b*h,mArea)
+        return mArea                                                      
+            
