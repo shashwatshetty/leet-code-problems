ec714c535bb1c575489b4e36973f76c70615eed0
sha5hw4t <shashwat_shetty@hotmail.com>
2017-12-26 15:45:39 -0500

    removeDuplicates added, runtime: 92ms

--- easy problems/removeDuplicates.py
+++ easy problems/removeDuplicates.py
@@ -1,15 +1,42 @@
+# Took 2 attempts!
 class Solution(object):
+    # brute force method
+    '''
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
         length = len(nums)
+        if length == 0:
+            return 0
+        i = 1
         prev = nums[0]
-        duplicates = 0
-        for i in range(1, len(nums) - 1):
-        	if nums[i] == prev:
-        		nums[i] = nums[i + 1]
-        		duplicates += 1
-        	prev = nums[i]
-        return (length - duplicates)
+        while i <= length - 1:
+            if nums[i] == prev:
+                for j in range(i, length):
+                    nums[j - 1] = nums[j]
+                length -= 1
+            else:
+                prev = nums[i]
+                i += 1
+        return length
+    '''
+
+    # simpler method
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        if length == 0:
+            return 0
+        pos = 1
+        i = 1
+        while i < length:
+            if nums[i] != nums[i - 1]:
+                nums[pos] = nums[i]
+                pos += 1
+            i += 1
+        return pos
