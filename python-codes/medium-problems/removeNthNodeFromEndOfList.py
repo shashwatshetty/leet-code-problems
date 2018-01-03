1e0f096dd326e7fe4ee87cd3b9d30194b8f3848f
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-03 17:03:08 -0500

    removeNthNodeFromEndOfList added, runtime: 55ms

---
+++ python-codes/medium-problems/removeNthNodeFromEndOfList.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+#Took 2 attempts!
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        nodes = []
+        while head is not None:
+            nodes.append(head)
+            head = head.next
+        last = len(nodes)
+        remove = last - n
+        if remove == 0:
+            return nodes[0].next
+        toRemove = nodes[remove]
+        nodes[remove - 1].next = toRemove.next
+        return nodes[0]
+
+
+# Test Cases Data
+'''
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+m = ListNode(4)
+n = ListNode(5)
+o = ListNode(6)
+a.next = b
+b.next = c
+c.next = m
+m.next = n
+n.next = o
+'''
