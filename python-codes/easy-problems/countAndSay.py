6503f31cc8349d27ee3005074c2c545fae4b1a66
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-06 22:38:44 -0500

    countAndSay added, runtime: 52ms

--- python-codes/easy-problems/countAndSay.py
+++ python-codes/easy-problems/countAndSay.py
@@ -7,20 +7,22 @@ class Solution(object):
         seq = '1'
         line = 1
         while line < n:
-            if len(seq) == 1:
-                new = '11'
             new = ''
             i = 1
             current = seq[0]
             count = 1
-            while i < len(seq):
+            while i <= len(seq):
+                if (i + 1) > len(seq):
+                    new += (str(count) + current)
+                    break
                 if seq[i] != current:
                     new += (str(count) + current)
                     current = seq[i]
                     count = 1
                 else:
                     count += 1
+                i += 1
             seq = new
-            print seq
-            l += 1
+            #print seq
+            line += 1
         return seq
