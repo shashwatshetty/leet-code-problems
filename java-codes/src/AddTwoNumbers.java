5ae5a7fd7faae3a54185772ab9425b714d73a311
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-18 20:43:05 -0500

    added comments to AddTwoNumbers

--- java-codes/src/AddTwoNumbers.java
+++ java-codes/src/AddTwoNumbers.java
@@ -9,24 +9,30 @@ class ListNode {
 }
 
 class AddTwoNumbersSolution {
+    /*
+    Basic idea is, that we can add two numbers in any order i.e. left to right
+    or right to left, to get the result, only maintaining the carry is important.
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode start = new ListNode(0);
-        ListNode head = start;
-        int carry = 0;
+        ListNode start = new ListNode(0);       // start with a temporary ListNode
+        ListNode head = start;                     // used to store the head of the list
+        int carry = 0;                             // used to store any carry
+        // first two conditions are to check if the list is empty or no
+        // third condition checks if after all additions there is a carry or no
         while(l1 != null || l2 != null || carry != 0){
             if(l1 != null){
-                carry += l1.val;
-                l1 = l1.next;
+                carry += l1.val;    // add into carry
+                l1 = l1.next;       // move to next digit
             }
             if(l2 != null){
-                carry += l2.val;
-                l2 = l2.next;
+                carry += l2.val;    // add into carry
+                l2 = l2.next;       // move to next digit
             }
-            start.next = new ListNode(carry % 10);
-            carry = carry / 10;
+            start.next = new ListNode(carry % 10);      // represents the last digit, units place in carry
+            carry = carry / 10;                            // reduce carry to whatever is in the tens place
             start = start.next;
         }
-        return head.next;
+        return head.next;       // move to the next since temp has leading 0
     }
 }
 
