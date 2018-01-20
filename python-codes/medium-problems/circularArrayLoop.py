a05d50b97930c9b3824a338a9f2c48e0b806503b
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-20 01:26:15 -0500

    trying different method, still working on logic

--- python-codes/medium-problems/circularArrayLoop.py
+++ python-codes/medium-problems/circularArrayLoop.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    '''
     def circularArrayLoop(self, nums):
         """
         :type nums: List[int]
@@ -19,3 +20,41 @@ class Solution(object):
             i += newI
             #print i
         return True
+    '''
+
+    def circularArrayLoop(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        length = len(nums)
+        if length < 2:
+            return False
+        while True:
+            slow = 0
+            fast = 0
+            tempSlow = (slow + nums[slow])
+            if tempSlow < 0:
+                tempSlow = length - tempSlow
+            else:
+                tempSlow = tempSlow % length
+            tempFast = (fast + nums[fast]) % length
+            tempFast = (tempFast + nums[tempFast]) % length
+            if tempSlow == slow or tempFast == fast:
+                return False
+            elif tempSlow == tempFast:
+                return True
+            slow = tempSlow
+            fast = tempFast
+
+    def updateIndex(self, index, nums):
+        """
+        :param index: int
+        :param nums: List[int]
+        :return: int
+        """
+        temp = index + nums[index]
+        if temp < 0:
+            return len(nums) + temp
+        else:
+            return temp % len(nums)
