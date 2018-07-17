fd9b8707b3772a92f3d451e54faaaea8551ce82a
sha5hw4t <shashwat_shetty@hotmail.com>
2018-07-17 15:57:19 -0400

    LengthOfLastWord added, runtime: 3ms

---
+++ java-codes/src/easyproblems/LengthOfLastWord.java
@@ -0,0 +1,43 @@
+package easyproblems;
+
+// Took 2 attmepts
+// Time Taken To Solve: 20 mins
+class LengthOfLastWordSolution {
+	public int lengthOfLastWord(String s){
+    	if(s == null){
+    		return 0;
+    	}
+    	s = s.trim();
+        int len = s.length() - 1;
+        int count = 0;
+        while(len >= 0){
+        	if(s.charAt(len) == ' '){
+        		return count;
+        	}
+        	count++;
+        	len--;
+        }
+        return count;
+    }
+    
+    /*
+    public int lengthOfLastWord(String s){
+    	s = s.trim();
+    	String[] words = s.split(" ");
+    	int n = words.length - 1;
+    	if(n < 0){
+    		return 0;
+    	}else{
+    		return words[n].length();
+    	}
+    }
+    */
+}
+
+public class LengthOfLastWord{
+	public static void main(String args[]){
+		String n = "hello    ";
+		LengthOfLastWordSolution obj = new LengthOfLastWordSolution();
+		System.out.println(obj.lengthOfLastWord(n));
+	}
+}
