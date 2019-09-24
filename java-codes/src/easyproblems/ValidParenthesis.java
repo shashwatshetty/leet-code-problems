972e8339036b6556520a5f4ddfde4620b295019d
Shashwat <shashwat_shetty@hotmail.com>
2019-09-23 22:50:47 -0400

    ValidParenthesis re-attempted, runtime: 2ms

--- java-codes/src/easyproblems/ValidParenthesis.java
+++ java-codes/src/easyproblems/ValidParenthesis.java
@@ -3,7 +3,8 @@ package easyproblems;
 import java.util.Stack;
 import java.util.HashMap;
 
-// Re-attempted Question
+// Re-Done in 1 attempt
+// Time Taken: 20 mins
 class ValidParenthesisSolution {
     public boolean isValid(String s) {
     	if(s == null || s.length() == 0){
@@ -36,6 +37,32 @@ class ValidParenthesisSolution {
 		}
 		return false;
     }
+
+	public boolean isValidREDO(String s) {
+		if (s == null || s.length() == 0) {
+			return true;
+		}
+		HashMap<Character, Character> brackets = new HashMap<>();
+		brackets.put(')', '(');
+		brackets.put('}', '{');
+		brackets.put(']', '[');
+		Stack<Character> parenthStack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			char p = s.charAt(i);
+			if (brackets.get(p) == null) {
+				parenthStack.push(p);
+			} else {
+				if (parenthStack.isEmpty() || parenthStack.pop() != brackets.get(p)) {
+					return false;
+				}
+			}
+		}
+		if (!parenthStack.isEmpty()) {
+			return false;
+		}
+		return true;
+	}
+
 }
 
 public class ValidParenthesis{
